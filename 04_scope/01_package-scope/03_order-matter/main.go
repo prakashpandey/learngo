5325d4994e8fb42a57e44dfa8484e1367b5d7100
Prakash Pandey <pcpandey@mail.com>
2018-09-11 16:43:40 +0530

    Variable order matters

---
+++ 04_scope/01_package-scope/03_order-matter/main.go
@@ -0,0 +1,19 @@
+/**
+ * @author Prakash Pandey
+ * @email prakashpandeyy@yahoo.com
+ * @github https://github.com/prakashpandey
+ * @create date 2018-09-11 04:28:30
+ * @modify date 2018-09-11 04:28:30
+ * @desc [description]
+ */
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(y) // should give compilation error
+	y := 100
+}
